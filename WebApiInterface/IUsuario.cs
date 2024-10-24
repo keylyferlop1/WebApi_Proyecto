011dbf04a89a7ba1e10f7b6afd7ff9976148b2be
keylyferlop1 <keyferlopgue 1357@gmail.com>
2024-10-24 22:09:37 +0200

    master

--- WebApiInterface/IUsuario.cs
+++ WebApiInterface/IUsuario.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebApiModels;
 
 namespace WebApiInterface
 {
     public interface IUsuario
     {
+        Task<IEnumerable<Usuarios>> GetUsuario();
 
+        Task<Usuarios> GetUsuarioAsync(int ID);
+
+        Task createUsuarioAsync(Usuarios usuarios);
+
+        Task updateUsuarioAsync(Usuarios usuarios);
+
+        Task DeleteUsuarioAsysnc(Usuarios usuarios);
     }
 }
