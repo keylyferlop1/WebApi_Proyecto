5e9747c9506a46f64ab15202f99d2c89840beaed
keylyferlop1 <keyferlopgue 1357@gmail.com>
2024-10-24 19:35:02 +0200

    main

---
+++ WebApiModels/Productos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiModels
+{
+    internal class Productos
+    {
+        public int ID_Producto {  get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public bool Estado { get; set;}
+    }
+}
---
+++ WebApiModels/Usuarios.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiModels
+{
+    internal class Usuarios
+    {
+        public int ID_Usuario { get; set; }
+        public string Usuario { get; set; }
+        public string clave { get; set; }
+        public string Rol { get; set; }
+        public bool Estado { get; set; }
+    }
+}
